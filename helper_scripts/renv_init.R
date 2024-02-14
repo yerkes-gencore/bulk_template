d7e5140205e4efb32404b6d0e9573586af3b170c
Derrik Gratz <46037149+derrik-gratz@users.noreply.github.com>
2024-02-14 13:36:27 -0800

    Create renv_init.R

---
+++ helper_scripts/renv_init.R
@@ -0,0 +1,31 @@
+## Cannot be sourced / ran as background due to need to restart R
+if (!require('renv')) {
+  install.packages('renv')
+}
+library(renv)
+
+if (!require('BiocManager')) {
+  install.packages('BiocManager')
+}
+
+## Link for non-locked CRAN
+## "https://cran.r-project.org" 
+CRAN_mirror <- "https://packagemanager.posit.co/cran/__linux__/jammy/2023-10-30"
+github_repos <- c('https://github.com/yerkes-gencore/gencoreBulk')
+bioconductor <- TRUE ## you can also set this to a specific version
+
+repos <- BiocManager::repositories()
+repos["CRAN"] <- CRAN_mirror
+options(repos=repos)
+
+init_settings <- list(
+  snapshot.type = 'implicit', ## explicit = Use the description file to initialize the project
+  use.cache = TRUE,
+  ignored.packages = c()      ## Specify any packages to exclude from renv
+)
+
+renv::init(bioconductor = bioconductor, # repos = c(CRAN_mirror, github_repos), repos = repos
+           settings = init_settings, bare = FALSE,
+           load = TRUE, restart = TRUE, force = FALSE)
+renv::activate()
+renv::snapshot()
